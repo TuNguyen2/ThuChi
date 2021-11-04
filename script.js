a82eb20bf4b7a2b93bfb2db5730da40ab6b00031
Nguyễn Văn Anh Tú <93466129+TuNguyen2@users.noreply.github.com>
2021-11-04 23:02:58 +0700

    ThuChi

---
+++ script.js
@@ -0,0 +1,46 @@
+var du = 0; 
+du = parseFloat(du);
+
+function laygitricu ()
+{
+    var gtc = document.getElementById("lichsu").value
+    return gtc;
+}
+
+function button()
+{
+    var tien    = document.getElementById("sotien").value 
+    var loai    = document.getElementById("loai").value
+    var ngay    = document.getElementById("ngay").value
+    var ghichu  = document.getElementById("ghichu").value 
+    var lichsu  = document.getElementById("lichsu");
+
+    
+    if (tien == "")
+    {
+        alert("Hãy nhập số tiền vào!!!")
+    } else if (ngay == "")
+    {
+        alert("Hãy chọn ngày!!!")
+    }  else if (ghichu == "")
+    {
+        alert("Hãy nhập ghi chú vào. Chứ không nhớ là tiền đó đâu ra đâu!!!")
+    }  else
+    { 
+        var text =  "" + loai  + "; " + ngay + "; " + tien + " (" + ghichu + ")"  +"\n"+ laygitricu()  ; 
+        lichsu.value = text;
+    }
+    // Số dư hiện tại
+    if (loai == "Thu Nhập")
+    {   tien = parseFloat(tien)
+        du = du + tien;
+        document.getElementById("sodu").innerHTML = du;
+        
+        
+    } else if (loai == "Chi Phí")
+    {   tien = parseFloat(tien)
+        du = du - tien;
+        document.getElementById("sodu").innerHTML =  du;
+    }
+}
+
